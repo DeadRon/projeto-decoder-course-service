c4d8d02e32731b892954ced20dbd96ce98fb4111
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-02-17 17:56:09 -0300

    [FEATURE] - dto para rebecer os dados do usuário criado no authuser via rabbitmq

---
+++ src/main/java/com/ead/course/dtos/UserEventDTO.java
@@ -0,0 +1,32 @@
+package com.ead.course.dtos;
+
+import com.ead.course.models.UserModel;
+import lombok.Data;
+import org.apache.catalina.User;
+import org.springframework.beans.BeanUtils;
+
+import java.util.UUID;
+
+@Data
+public class UserEventDTO {
+
+    private UUID userId;
+    private String username;
+    private String email;
+    private String fullName;
+    private String userStatus;
+    private String userType;
+    private String password;
+    private String phoneNumber;
+    private String oldPassword;
+    private String cpf;
+    private String imageUrl;
+    private String actionType;
+
+    public UserModel convertToUserModel(){
+        var userModel = new UserModel();
+        userModel.setId(userId);
+        return userModel;
+    }
+
+}
