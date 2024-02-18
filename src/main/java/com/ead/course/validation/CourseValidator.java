0ed6d82c3012abe0aa1ab1003ad21a21de0b1cda
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-02-18 16:54:03 -0300

    [FEATURE][SEMANA 5][Fluxo Híbrido de Comunicação Síncrona e Assíncron] valida dados de usuário instrutor antes de salvar

--- src/main/java/com/ead/course/validation/CourseValidator.java
+++ src/main/java/com/ead/course/validation/CourseValidator.java
@@ -2,6 +2,9 @@ package com.ead.course.validation;
 
 
 import com.ead.course.dtos.CourseDto;
+import com.ead.course.enums.UserType;
+import com.ead.course.models.UserModel;
+import com.ead.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Component
@@ -17,6 +21,9 @@ public class CourseValidator implements Validator {
     @Autowired
     @Qualifier("defaultValidator")
     private Validator validator;
+
+    @Autowired
+    private UserService userService;
 
     @Override
     public boolean supports(Class<?> aClass) {
@@ -33,6 +40,16 @@ public class CourseValidator implements Validator {
     }
 
     private void validateUserInstructor(UUID userInstructor, Errors errors){
+
+        Optional<UserModel> userModelOptional = userService.findById(userInstructor);
+        if(userModelOptional.isEmpty()){
+            errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not found.");
+        }
+
+        if(userModelOptional.get().getUserType().equals(UserType.STUDENT.toString())){
+            errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN");
+        }
+
 //        ResponseEntity<UserDto> responseUserInstructor;
 //        try {
 //            responseUserInstructor = authUserClient.getOneUserById(userInstructor);
