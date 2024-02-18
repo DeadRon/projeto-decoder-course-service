011cf679977e79a3e858181ca74706cd564c15bb
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-02-18 16:30:29 -0300

    [FEATURE][SEMANA 5][Fluxo Híbrido de Comunicação Síncrona e Assíncrona ] validações para inscrever aluno em um curso e salvar

--- src/main/java/com/ead/course/controllers/CourseUserController.java
+++ src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -1,11 +1,15 @@
 package com.ead.course.controllers;
 
 import com.ead.course.dtos.SubscriptionDto;
+import com.ead.course.enums.UserStatus;
 import com.ead.course.models.CourseModel;
+import com.ead.course.models.UserModel;
 import com.ead.course.services.CourseService;
 import com.ead.course.services.UserService;
+import com.ead.course.specifications.SpecificationTemplate;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -29,14 +33,18 @@ public class CourseUserController {
     UserService userService;
 
     @GetMapping("/courses/{courseId}/users")
-    public ResponseEntity<Object> getAllUsersByCourse(@PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
+    public ResponseEntity<Object> getAllUsersByCourse(SpecificationTemplate.UserSpec spec,
+                                                      @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
                                                       @PathVariable(value = "courseId") UUID courseId){
 
         Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
         if(!courseModelOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found.");
         }
-        return ResponseEntity.status(HttpStatus.OK).body("");
+
+        Page<UserModel> userServiceAll = userService.findAll(SpecificationTemplate.userCourseId(courseId).and(spec), pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(userServiceAll);
     }
 
     @PostMapping("/courses/{courseId}/users/subscription")
@@ -47,7 +55,26 @@ public class CourseUserController {
         if(!courseModelOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found.");
         }
+
         //verificações state transfer
+        //Verifica se o usuário STUDENT não esta cadastrado no curso
+        if(courseService.existsByCourseAndUser(courseId,  subscriptionDto.getUserId())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: subscription already exists!");
+        }
+
+        //verificar se usuário existe
+        Optional<UserModel> userModelOptional = userService.findById(subscriptionDto.getUserId());
+        if(!userModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        //verificar se usuário está Bloqueado através do userStatus
+        if(userModelOptional.get().getUserStatus().equals(UserStatus.BLOCKED.toString())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: subscription already exists!");
+        }
+        courseService.saveSubscriptionUserInCourse(courseModelOptional.get().getCourseId(), userModelOptional.get().getUserId());
+
+        log.info("[COURSE USER CONTROLLER] User {} subscripited in course {}.", subscriptionDto.getUserId(), courseId);
         return ResponseEntity.status(HttpStatus.CREATED).body("");
     }
 }
