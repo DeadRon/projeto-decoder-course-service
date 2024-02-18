6a302084ec3616fa2356a996909562310bf09be6
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-02-18 16:45:42 -0300

    [FEATURE][SEMANA 5][Fluxo Híbrido de Comunicação Síncrona e Assíncrona] associa estudante a curso e verificar se aluno já não esta inscrito

--- src/main/java/com/ead/course/repositories/CourseRepository.java
+++ src/main/java/com/ead/course/repositories/CourseRepository.java
@@ -3,8 +3,33 @@ package com.ead.course.repositories;
 import com.ead.course.models.CourseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface CourseRepository extends JpaRepository<CourseModel, UUID>, JpaSpecificationExecutor<CourseModel> {
+
+    /*
+    este comando é usado para checar se existe um registro na tabela tb_courses_users que associa um determinado
+    curso (identificado por courseId) a um determinado usuário (identificado por userId), retornando true
+    se o registro existir e false se não existir.
+    */
+    @Query(
+            value = "SELECT CASE WHEN COUNT(tcu) > 0 THEN true ELSE false END FROM tb_courses_users tcu " +
+                    "WHERE tcu.course_id= :courseId " +
+                    "AND tcu.user_id= :userId",
+            nativeQuery = true
+    )
+    boolean existsByCourseAndUser(@Param("courseId") UUID courseId, @Param("userId") UUID userId);
+
+    //associa um estudante ha um curso
+    @Modifying
+    @Query(
+            value = "INSERT INTO tb_courses_users VALUES (:courseId, :userId);",
+            nativeQuery = true
+    )
+    void saveCourseUser(@Param("courseId") UUID courseId, @Param("userId") UUID userId);
+
 }
