eb0bbbcebbc92db9b8f285c862b7d652923372cc
Ronaldo Melo <ronaldorpm1994@gmail.com>
2024-02-18 20:03:52 -0300

    [FEATURE][SEMANA 5][Fluxo Híbrido de Comunicação Síncrona e Assíncron] cancela a associação de um usuário em um curso

--- src/main/java/com/ead/course/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/UserServiceImpl.java
@@ -1,22 +1,29 @@
 package com.ead.course.services.impl;
 
 import com.ead.course.models.UserModel;
+import com.ead.course.repositories.CourseRepository;
 import com.ead.course.repositories.UserRepository;
 import com.ead.course.services.UserService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Log4j2
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    CourseRepository courseRepository;
 
     @Override
     public Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable) {
@@ -28,9 +35,13 @@ public class UserServiceImpl implements UserService {
         return userRepository.save(userModel);
     }
 
+    @Transactional
     @Override
     public void delete(UUID userId) {
+        courseRepository.deleteCourseUserByUser(userId);
+        log.info("[USER SERVICE] deleted user from course {}", userId);
         userRepository.deleteById(userId);
+        log.info("[USER SERVICE] deleted user {}", userId);
     }
 
     @Override
